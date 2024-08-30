6cc6ab972a74795d0e010c369a02cf11a009c4f5
Andrew <work.andrew.naida@gmail.com>
2024-08-30 19:36:01 +0200

    Edit user functionality

--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -4,7 +4,6 @@ export const Navbar = () => {
     return (
         <div className="flex flex-col gap-4">
             <Link href="/dashboard">Dashboard</Link>
-            <Link href="/dashboard/personal-info">Personal Info</Link>
             <Link href="/dashboard/events">Events</Link>
             <Link href="/dashboard/group">Your groups</Link>
             <Link href="/dashboard/interests">Interests</Link>
--- src/components/dashboard/UserProfileSection.tsx
+++ src/components/dashboard/UserProfileSection.tsx
@@ -1,21 +1,111 @@
-import { Modal } from "@/features/Modal";
+"use client"
+
 import { useUserContext } from "@/providers/UserContextProvider";
 import { Database } from "@/types/supabase";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { Toaster } from "../ui/toaster";
 import { UserDataModal } from "./UserDataModal";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import { UserData } from "@/types/types";
+import { toast } from "../ui/use-toast";
+import { Modal } from "@/features/Modal";
 
 
 
 export const UserProfileSection = () => {
     const supabase = createClientComponentClient<Database>();
+    const queryClient = useQueryClient();
     const { userRole, userId } = useUserContext();
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [city, setCity] = useState("");
+    const [country, setCountry] = useState("");
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
+    const addUserData = useMutation(
+        async (newUserData: UserData[]) => {
+            await supabase
+                .from("users")
+                .upsert(newUserData)
+                .eq("id", userId);
+        },
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries(['users']);
+                toast({
+                    title: "Success",
+                    description: "User created successfully",
+                });
+
+            },
+
+            onError: () => {
+                toast({
+                    variant: "destructive",
+                    title: "Error",
+                    description: "There was an error creating the user"
+                });
+            }
+        }
+    );
 
     const bodyContent = (
         <>
-            <UserDataModal />
+            <Input
+                placeholder="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+            <Input
+                placeholder="Last Name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+            />
+            <Input
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+                placeholder="City"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+            />
+            <Input
+                placeholder="Country"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+            />
+            <Button onClick={() => {
+                if (!firstName || !lastName || !email || !city || !country) {
+                    toast({
+                        variant: "destructive",
+                        title: "Error",
+                        description: "Please fill out all fields"
+                    });
+                    return;
+                } else {
+                    addUserData.mutateAsync([{
+                        first_name: firstName,
+                        last_name: lastName,
+                        email: email,
+                        city: city,
+                        country: country,
+                        user_role: "User",
+                        id: userId
+                    }] as UserData[], {
+                        onSuccess: () => {
+                            queryClient.invalidateQueries(['users']);
+
+                            setIsModalOpen(false);
+                        }
+                    });
+                }
+            }}>Edit Profile</Button>
         </>
     )
 
@@ -40,9 +130,6 @@ export const UserProfileSection = () => {
 
     return (
         <>
-
-
-
             <div>
                 {getUserData.data?.map((user) => (
                     <div key={user.id}>
@@ -54,12 +141,17 @@ export const UserProfileSection = () => {
                 {getUserData.isLoading && <div>Loading...</div>}
             </div>
 
+            <Button onClick={() => { setIsModalOpen(true) }}>Edit Profile</Button>
+
+            <Modal title="Edit your profile"
+                body={bodyContent}
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onChange={setIsModalOpen}
+            />
+
             {!userRole && (
-                <Modal title="Create User"
-                    body={bodyContent}
-                    isOpen={true}
-                    onClose={() => { }}
-                />
+                <UserDataModal />
             )}
 
             <Toaster />
