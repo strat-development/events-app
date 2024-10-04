538c3ca659b77030299f8630cfd5e743e19ac826
Andrew <work.andrew.naida@gmail.com>
2024-10-04 12:24:10 +0200

    Events filtering fix

--- src/features/events-main-page/EventsSection.tsx
+++ src/features/events-main-page/EventsSection.tsx
@@ -5,7 +5,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
 import { useQuery } from "react-query";
 import stringSimilarity from "string-similarity";
-import { format, startOfToday, parseISO, isSameDay } from "date-fns";
+import { format, startOfToday, parseISO } from "date-fns";
 import { Calendar } from "@/components/ui/calendar";
 import Link from "next/link";
 
@@ -15,8 +15,12 @@ export const EventsSection = () => {
     const [events, setEvents] = useState<{ [date: string]: EventData[] }>({});
     const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
+    console.log('userInterests:', userInterests);
+
     const handleDateChange = (date: Date) => {
         setSelectedDate(date);
+
+        console.log('selectedDate:', date);
     };
 
     useQuery(
@@ -29,46 +33,42 @@ export const EventsSection = () => {
             const { data, error } = await supabase
                 .from("events")
                 .select("*")
-                .gte('starts_at', today);
+                .gte('starts_at', selectedDate?.toISOString() || today);
 
             if (error) {
                 console.error('Error fetching events:', error);
                 throw new Error(error.message);
             }
 
-            const exactMatches = data.filter((event: EventData) => {
-                const eventTopics = event.event_topics as { interests: { name: string }[] };
-                return eventTopics?.interests.some((interest: { name: string }) =>
-                    userInterests.includes(interest.name)
-                );
-            });
-
-            const similarityThreshold = 0.1;
-            const similarMatches = exactMatches.length > 0 ? exactMatches : data.filter((event: EventData) => {
-                const eventTopics = event.event_topics as { interests: { name: string }[] };
-                return eventTopics && Array.isArray(eventTopics.interests) && eventTopics.interests.some((interest: { name: string }) => {
-                    const similarityScores = userInterests.map(userInterest =>
-                        stringSimilarity.compareTwoStrings(
-                            interest.name.toLowerCase(),
-                            userInterest.toLowerCase()
-                        )
+            if (data) {
+                const exactMatches = data.filter((event: EventData) => {
+                    const eventTopics = event.event_topics as { interests: { name: string }[] };
+                    return eventTopics?.interests.some((interest: { name: string }) =>
+                        userInterests.includes(interest.name)
                     );
-                    return Math.max(...similarityScores) >= similarityThreshold;
                 });
-            });
 
-            const filteredEvents = selectedDate
-                ? similarMatches.filter((event: EventData) =>
-                    parseISO(event.starts_at as string) >= selectedDate
-                )
-                : similarMatches;
+                const similarityThreshold = 0.5;
+                const similarMatches = exactMatches.length > 0 ? exactMatches : data.filter((event: EventData) => {
+                    const eventTopics = event.event_topics as { interests: { name: string }[] };
+                    return eventTopics && Array.isArray(eventTopics.interests) && eventTopics.interests.some((interest: { name: string }) => {
+                        const similarityScores = userInterests.map(userInterest =>
+                            stringSimilarity.compareTwoStrings(
+                                interest.name.toLowerCase(),
+                                userInterest.toLowerCase()
+                            )
+                        );
+                        return Math.max(...similarityScores) >= similarityThreshold;
+                    });
+                });
 
-            return filteredEvents;
-        },
-        {
-            enabled: !!userInterests && userInterests.length > 0,
-            onSuccess: (data) => {
-                const groupedEvents = data.reduce((acc: { [date: string]: EventData[] }, event: EventData) => {
+                const filteredEvents = selectedDate
+                    ? similarMatches.filter((event: EventData) =>
+                        parseISO(event.starts_at as string) >= selectedDate
+                    )
+                    : similarMatches;
+
+                const groupedEvents = filteredEvents.reduce((acc: { [date: string]: EventData[] }, event: EventData) => {
                     const eventDate = format(parseISO(event.starts_at as string), 'yyyy-MM-dd');
                     if (!acc[eventDate]) {
                         acc[eventDate] = [];
@@ -76,22 +76,28 @@ export const EventsSection = () => {
                     acc[eventDate].push(event);
                     return acc;
                 }, {});
+
                 setEvents(groupedEvents);
             }
+        },
+        {
+            enabled: !!userInterests && userInterests.length > 0,
         }
     );
 
     return (
         <>
-            <div className="flex gap-16">
+            <div className="flex gap-16 items-center">
                 <Calendar onDayClick={handleDateChange} />
                 <div className="flex flex-col">
-                    {Object.keys(events).map((date) => (
-                        <div key={date}>
+                    <h1>EVENTS</h1>
+                    {Object.entries(events).map(([date, events]) => (
+                        <div key={date} className="flex flex-col gap-8">
                             <h2 className="text-xl font-bold">{date}</h2>
-                            {events[date].map((event) => (
-                                <Link href={`/event-page/${event.id}`} 
-                                key={event.id}>{event.event_title}</Link>
+                            {events.map((event) => (
+                                <Link key={event.id} href={`/event-page/${event.id}`}>
+                                    {event.event_title}
+                                </Link>
                             ))}
                         </div>
                     ))}
